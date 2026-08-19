1b4900b01eae4a551ed317244f6ecd0e13f9b92e
vijaykumar <pawarvijaykumar821@gmail.com>
2026-08-19 19:16:42 +0530

    second commit

---
+++ 01basicreact/src/App.jsx
@@ -0,0 +1,12 @@
+import vijay from './vijay.jsx'
+//function vijay(){
+function App() {
+  return (
+    <>
+    <h2>the boss is vijay</h2>
+    <pr>hey boi</pr>
+    </>
+  );
+};
+    
+export default App;
---
+++ 01basicreact/src/vijay.jsx
@@ -0,0 +1,8 @@
+//import vijay from './vijay'
+function vijay(){
+  return(
+    <h1>the vijay is boss</h1>
+
+  );
+};
+export default vijay;
