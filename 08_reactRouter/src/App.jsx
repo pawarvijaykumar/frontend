b223cab463673da2df5f6f8abb211c2fc966d695
vijaykumar <pawarvijaykumar821@gmail.com>
2026-08-27 17:01:12 +0530

    raect router

---
+++ 08_reactRouter/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h2 className='bg-green-600 p-4'>king</h2>
+    </>
+  )
+}
+
+export default App
