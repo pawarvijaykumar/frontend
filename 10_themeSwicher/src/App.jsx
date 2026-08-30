261a215f543fc137fc63cb5010259a7ee72b7a49
vijaykumar <pawarvijaykumar821@gmail.com>
2026-08-30 18:33:16 +0530

    context

---
+++ 10_themeSwicher/src/App.jsx
@@ -0,0 +1,25 @@
+
+function App() {
+  
+  return (
+    <>
+    <div>vijay
+    
+<div className="flex flex-wrap min-h-screen items-center">
+                <div className="w-full">
+                    <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
+                        
+                    </div>
+
+                    <div className="w-full max-w-sm mx-auto">
+                       
+                    </div>
+                </div>
+            </div>
+            </div>
+
+      </>
+  )
+}
+
+export default App
