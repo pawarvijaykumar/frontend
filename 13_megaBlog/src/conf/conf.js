7226ab8b374f5d912bd7da1ac4c14ac6de43ee14
vijaykumar <pawarvijaykumar821@gmail.com>
2026-09-04 23:22:10 +0530

    appwrite

---
+++ 13_megaBlog/src/conf/conf.js
@@ -0,0 +1,8 @@
+const conf={
+  appwriteUrl:String(import.meta.env.VITE_APPWRITE_URL),
+  appwritedatabaseId:String(import.meta.env.VITE_DATABASE_ID),
+  appwriteProjectId:String(import.meta.env.VITE_PROJECT_ID),
+  appwriteTableId:String(import.meta.env.VITE_TABLE_ID),
+  appwriteBucketId:String(import.meta.env.VITE_BUCKET_ID),
+  
+}
