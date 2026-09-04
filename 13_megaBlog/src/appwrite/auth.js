70353bc8ec1b6a023ddd01101872ed2a8e1f2df2
vijaykumar <pawarvijaykumar821@gmail.com>
2026-09-05 00:48:20 +0530

    authentificatin

---
+++ 13_megaBlog/src/appwrite/auth.js
@@ -0,0 +1,48 @@
+import { Account, ID } from "appwrite";
+import { client } from "../conf/conf.js";
+
+const account = new Account(client);
+
+// Signup / Create Account
+async function createAccount(email, password, name) {
+  try {
+    const response = await account.create(ID.unique(), email, password, name);
+    return response;
+  } catch (error) {
+    console.log("Appwrite service :: createAccount :: error", error);
+    throw error;
+  }
+}
+
+// Login
+async function login(email, password) {
+  try {
+    const session = await account.createEmailPasswordSession(email, password);
+    return session;
+  } catch (error) {
+    console.log("Appwrite service :: login :: error", error);
+    throw error;
+  }
+}
+
+// Current logged-in user check
+async function getCurrentUser() {
+  try {
+    return await account.get();
+  } catch (error) {
+    console.log("Appwrite service :: getCurrentUser :: error", error);
+  }
+  return null;
+}
+
+// Logout
+async function logout() {
+  try {
+    await account.deleteSession("current");
+  } catch (error) {
+    console.log("Appwrite service :: logout :: error", error);
+  }
+}
+
+
+export { createAccount, login, getCurrentUser, logout };
